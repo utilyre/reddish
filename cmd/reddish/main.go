dbe58fd027f522f10caf1080c8b2ee2ef045653a
Amirabbas Fazelinia <utilyre@gmail.com>
2024-06-25 12:03:36 +0330

    feat: set default logger

--- cmd/reddish/main.go
+++ cmd/reddish/main.go
@@ -1,19 +1,36 @@
 package main
 
 import (
-	"log"
+	"errors"
+	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/utilyre/reddish/internal/adapters/mapstorage"
 	"github.com/utilyre/reddish/internal/adapters/rpc"
 	"github.com/utilyre/reddish/internal/app/service"
 )
 
+func init() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
+
+	slog.SetDefault(logger)
+}
+
 func main() {
 	storageRepo := mapstorage.NewMapStorage()
 	storageSVC := service.NewStorageService(storageRepo)
 	storageHandler := rpc.NewStorageHandler(storageSVC)
 
-	storageSRV := rpc.NewStorageServer(storageHandler)
-	log.Fatal(http.ListenAndServe(":5000", storageSRV))
+	srv := &http.Server{
+		Addr:    ":5000",
+		Handler: rpc.NewStorageServer(storageHandler),
+	}
+	slog.Info("starting storage server", "address", srv.Addr)
+	if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		slog.Error("failed to start storage server", "address", srv.Addr, "error", err)
+		os.Exit(1)
+	}
 }
