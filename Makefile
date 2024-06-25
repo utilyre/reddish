266a8947ad39918e796329bb57ba4198b8623f49
Amirabbas Fazelinia <utilyre@gmail.com>
2024-06-25 11:57:30 +0330

    build(go): use `go generate` command to replace makefile

--- Makefile
+++
@@ -1,4 +0,0 @@
-.PHONY: protogen
-
-protogen:
-	protoc --go_out=paths=source_relative:. --twirp_out=paths=source_relative:. internal/adapters/rpc/*.proto
--- internal/adapters/rpc/storage_handler.go
+++ internal/adapters/rpc/storage_handler.go
@@ -1,5 +1,7 @@
 package rpc
 
+//go:generate protoc --go_out=paths=source_relative:. --twirp_out=paths=source_relative:. storage_service.proto
+
 import (
 	"context"
 
